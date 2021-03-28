7d2ed1b2fdfec58bca372f6a73439591e6ddd5a3
SBC <lochan.sharad@gmail.com>
2021-03-28 16:03:59 +0800

    Index,style and js file added. Required Html elements created and given style

---
+++ script.js
@@ -0,0 +1,25 @@
+var startbutton = document.getElementById('start-btn');
+var startbutton = document.getElementById('start-btn');
+var startbutton = document.getElementById('start-btn');
+var startbutton = document.getElementById('start-btn');
+var startbutton = document.getElementById('start-btn');
+var startbutton = document.getElementById('start-btn');
+
+
+startbutton.addEventListener('click', startquiz);
+
+
+function startquiz (){
+
+    
+}
+
+
+function setnextquestion(){
+
+}
+
+
+function selectanswer(){
+
+}
